4cc07da07d9e52b517189144e2b40658db8a2c89
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-11-28 23:23:51 +0600

    lms added

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.css">
 </head>
 
 <body>
-    <a href="student/">Students</a>
-    <a href="librarian/">Librarian</a>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+                <a class="btn btn-primary btn-block" href="student/">Students</a>
+                <a class="btn btn-primary btn-block" href="librarian/">Librarian</a>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
